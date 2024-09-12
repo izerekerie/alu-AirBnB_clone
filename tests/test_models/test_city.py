0aa31b1c46285d15a8a8487c9be81161f33c5389
carine-69 <umugabecarine@gmail.com>
2024-09-13 01:40:33 +0200

    testing city class and functions

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import unittest
+from models.city import City
+
+
+class CityTest(unittest.TestCase):
+    def test_properties(self):
+        city = City()
+        self.assertTrue(hasattr(city, 'name'))
+        self.assertTrue(hasattr(city, 'id'))
+        self.assertTrue(hasattr(city, 'created_at'))
+        self.assertTrue(hasattr(city, 'updated_at'))
+
+    def test_default_value(self):
+        city = City()
+        self.assertEqual(city.name, "")
+        self.assertIsNotNone(city.id)  # ID is usually auto-generated, so shouldn't be an empty string
+
+    def test_assigned_value(self):
+        city = City()
+        city.name = "Kigali"
+        city.id = "123"
+        self.assertEqual(city.name, "Kigali")
+        self.assertEqual(city.id, "123")
+
+
+if __name__ == '__main__':
+    unittest.main()
