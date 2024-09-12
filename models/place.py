a9a37d45d9a801ea152fd36819df2a7aea36e711
carine-69 <umugabecarine@gmail.com>
2024-09-13 00:28:45 +0200

    modified place.py to add some values

--- models/place.py
+++ models/place.py
@@ -6,10 +6,10 @@ class Place(BaseModel):
     user_id = ""    # string (user.id)
     name = ""   # string
     description = ""    # string
-    number_rooms = ""   # integer
-    number_bathrooms = ""   # integer
-    max_guest = ""  # integer
-    price_by_night = ""  # integer
-    latitude = ""   # float
-    longitude = ""  # float
+    number_rooms = 0   # integer
+    number_bathrooms = 0   # integer
+    max_guest = 0  # integer
+    price_by_night = 0  # integer
+    latitude = 0.00   # float
+    longitude = 0.00  # float
     amenity_ids = []    # list(Amenity.id)
