5239dd534cc2df848738ec151e5d3401d8d0b57a
carine-69 <umugabecarine@gmail.com>
2024-09-13 00:03:17 +0200

    created place class and many attribute

---
+++ models/place.py
@@ -0,0 +1,13 @@
+from models.base_model import BaseModel
+class Place(BaseModel):
+    city_id = ""    # string (city.id)
+    user_id = ""    # string (user.id)
+    name = ""   # string
+    description = ""    # string
+    number_rooms = ""   # integer
+    number_bathrooms = ""   # integer
+    max_guest = ""  # integer
+    price_by_night = ""  # integer
+    latitude = ""   # float
+    longitude = ""  # float
+    amenity_ids = []    # list(Amenity.id)
